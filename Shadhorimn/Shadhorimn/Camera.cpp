7455e6c513ef19ef2b1d28653dafa2b5312b97ce
ijames428 <ijames428@gmail.com>
2017-08-22 21:13:53 -0500

    Committing Camera files

---
+++ Shadhorimn/Shadhorimn/Camera.cpp
@@ -0,0 +1,8 @@
+using namespace std;
+#include <iostream>
+#include "Camera.h"
+
+Camera::Camera(sf::Vector2f position, sf::Vector2f dimensions) {
+	viewport_position = position;
+	viewport_dimensions = dimensions;
+}
